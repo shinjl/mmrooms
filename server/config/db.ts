4f604c7809dc188f42d1a1fa8d1fea990d95fb0c
Shinjl <info@shinjl.com>
2021-01-23 16:26:32 +0900

    refactor: deleted some test code

--- server/config/db.ts
+++ server/config/db.ts
@@ -2,7 +2,6 @@ import { MongoClient } from "./deps.ts";
 
 const dbName = Deno.env.get("DB_NAME") || "";
 const dbHostUrl = Deno.env.get("DB_HOST_URL") || "";
-console.log(dbHostUrl);
 
 const client = new MongoClient();
 await client.connect(dbHostUrl);
